7bd5638450a6dec0c64ee16664e42ad514f30ce8
vinotharanjanij2006-ship-it <vinotharanjanij2006@gmail.com>
2026-02-03 20:25:31 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+length=int(input("Enter password length:"))
+characters=string.ascii_letters+string.digits+string.punctuation
+password=""
+for i in range(length):
+    password+=random.choice(characters)
+print("Generated password:",password)
+
